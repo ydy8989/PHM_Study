edb84741dd3e0d5d30b5e75cfef43cfd6c3618b6
DoyeonYoon <38639633+ydy8989@users.noreply.github.com>
2019-07-21 12:48:41 +0900

    first commit

---
+++ Auto_Encoder/processminor.py
@@ -0,0 +1,279 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pandas as pd
+import numpy as np
+from pylab import rcParams
+
+import tensorflow as tf
+from keras import optimizers, Sequential
+from keras.models import Model
+from keras.utils import plot_model
+from keras.layers import Dense, LSTM, RepeatVector, TimeDistributed
+from keras.callbacks import ModelCheckpoint, TensorBoard
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, precision_recall_curve
+from sklearn.metrics import recall_score, classification_report, auc, roc_curve
+from sklearn.metrics import precision_recall_fscore_support, f1_score
+
+from numpy.random import seed
+seed(7)
+from tensorflow import set_random_seed
+set_random_seed(11)
+
+SEED = 123 #used to help randomly select the data points
+DATA_SPLIT_PCT = 0.2
+
+rcParams['figure.figsize'] = 8, 6
+LABELS = ["Normal","Break"]
+
+df = pd.read_csv("processminer-rare-event-mts - data.csv") 
+df.head(n=5)  # visualize the data.
+
+#df.ix[:,40:].plot(subplots = True, figsize = (10,40))
+
+sign = lambda x: (1, -1)[x < 0]
+
+def curve_shift(df, shift_by):
+    '''
+    This function will shift the binary labels in a dataframe.
+    The curve shift will be with respect to the 1s. 
+    For example, if shift is -2, the following process
+    will happen: if row n is labeled as 1, then
+    - Make row (n+shift_by):(n+shift_by-1) = 1.
+    - Remove row n.
+    i.e. the labels will be shifted up to 2 rows up.
+    
+    Inputs:
+    df       A pandas dataframe with a binary labeled column. 
+             This labeled column should be named as 'y'.
+    shift_by An integer denoting the number of rows to shift.
+    
+    Output
+    df       A dataframe with the binary labels shifted by shift.
+    '''
+    
+    vector = df['y'].copy()
+    for s in range(abs(shift_by)):
+        tmp = vector.shift(sign(shift_by))
+        tmp = tmp.fillna(0)
+        vector += tmp
+    labelcol = 'y'
+    # Add vector to the df
+    df.insert(loc=0, column=labelcol+'tmp', value=vector)
+    # Remove the rows with labelcol == 1.
+    df = df.drop(df[df[labelcol] == 1].index)
+    # Drop labelcol and rename the tmp col as labelcol
+    df = df.drop(labelcol, axis=1)
+    df = df.rename(columns={labelcol+'tmp': labelcol})
+    # Make the labelcol binary
+    df.loc[df[labelcol] > 0, labelcol] = 1
+
+    return df
+
+print('Before shifting')  # Positive labeled rows before shifting.
+one_indexes = df.index[df['y'] == 1]
+
+#display(df.iloc[(np.where(np.array(input_y) == 1)[0][0]-5):(np.where(np.array(input_y) == 1)[0][0]+1), ])
+
+# Shift the response column y by 2 rows to do a 4-min ahead prediction.
+df = curve_shift(df, shift_by = -2)
+
+print('After shifting')  # Validating if the shift happened correctly.
+display(df.iloc[(one_indexes[0]-4):(one_indexes[0]+1), 0:5].head(n=5))
+
+# Remove time column, and the categorical columns
+df = df.drop(['time', 'x28', 'x61'], axis=1)
+
+input_X = df.loc[:, df.columns != 'y'].values  # converts the df to a numpy array
+input_y = df['y'].values
+
+n_features = input_X.shape[1]  # number of features
+
+def temporalize(X, y, lookback):
+    X = []
+    y = []
+    for i in range(len(input_X)-lookback-1):
+        t = []
+        for j in range(1,lookback+1):
+            # Gather past records upto the lookback period
+            t.append(input_X[[(i+j+1)], :])
+        X.append(t)
+        y.append(input_y[i+lookback+1])
+    return X, y
+
+print('First instance of y = 1 in the original data')
+display(df.iloc[(np.where(np.array(input_y) == 1)[0][0]-5):(np.where(np.array(input_y) == 1)[0][0]+1), ])
+lookback = 5  # Equivalent to 10 min of past data.
+# Temporalize the data
+X, y = temporalize(X = input_X, y = input_y, lookback = lookback)
+print('For the same instance of y = 1, we are keeping past 5 samples in the 3D predictor array, X.')
+display(pd.DataFrame(np.concatenate(X[np.where(np.array(y) == 1)[0][0]], axis=0 )))
+
+X_train, X_test, y_train, y_test = train_test_split(np.array(X), np.array(y), test_size=DATA_SPLIT_PCT, random_state=SEED)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=DATA_SPLIT_PCT, random_state=SEED)
+
+X_train_y0 = X_train[y_train==0]
+X_train_y1 = X_train[y_train==1]
+X_valid_y0 = X_valid[y_valid==0]
+X_valid_y1 = X_valid[y_valid==1]
+
+X_train = X_train.reshape(X_train.shape[0], lookback, n_features)
+X_train_y0 = X_train_y0.reshape(X_train_y0.shape[0], lookback, n_features)
+X_train_y1 = X_train_y1.reshape(X_train_y1.shape[0], lookback, n_features)
+X_valid = X_valid.reshape(X_valid.shape[0], lookback, n_features)
+X_valid_y0 = X_valid_y0.reshape(X_valid_y0.shape[0], lookback, n_features)
+X_valid_y1 = X_valid_y1.reshape(X_valid_y1.shape[0], lookback, n_features)
+X_test = X_test.reshape(X_test.shape[0], lookback, n_features)
+
+def flatten(X):
+    '''
+    Flatten a 3D array.
+    
+    Input
+    X            A 3D array for lstm, where the array is sample x timesteps x features.
+    
+    Output
+    flattened_X  A 2D array, sample x features.
+    '''
+    flattened_X = np.empty((X.shape[0], X.shape[2]))  # sample x features array.
+    for i in range(X.shape[0]):
+        flattened_X[i] = X[i, (X.shape[1]-1), :]
+    return(flattened_X)
+
+def scale(X, scaler):
+    '''
+    Scale 3D array.
+
+    Inputs
+    X            A 3D array for lstm, where the array is sample x timesteps x features.
+    scaler       A scaler object, e.g., sklearn.preprocessing.StandardScaler, sklearn.preprocessing.normalize
+    
+    Output
+    X            Scaled 3D array.
+    '''
+    for i in range(X.shape[0]):
+        X[i, :, :] = scaler.transform(X[i, :, :])
+        
+    return X
+
+# Initialize a scaler using the training data.
+scaler = StandardScaler().fit(flatten(X_train_y0))
+
+X_train_y0_scaled = scale(X_train_y0, scaler)
+
+
+
+a = flatten(X_train_y0_scaled)
+print('colwise mean', np.mean(a, axis=0).round(6))
+print('colwise variance', np.var(a, axis=0))
+X_valid_scaled = scale(X_valid, scaler)
+X_valid_y0_scaled = scale(X_valid_y0, scaler)
+X_test_scaled = scale(X_test, scaler)
+timesteps =  X_train_y0_scaled.shape[1] # equal to the lookback
+n_features =  X_train_y0_scaled.shape[2] # 59
+#%%
+epochs = 200
+batch = 64
+lr = 0.0001
+
+lstm_autoencoder = Sequential()
+# Encoder
+lstm_autoencoder.add(LSTM(32, activation='relu', input_shape=(timesteps, n_features), return_sequences=True))
+lstm_autoencoder.add(LSTM(16, activation='relu', return_sequences=False))
+lstm_autoencoder.add(RepeatVector(timesteps))
+# Decoder
+lstm_autoencoder.add(LSTM(16, activation='relu', return_sequences=True))
+lstm_autoencoder.add(LSTM(32, activation='relu', return_sequences=True))
+lstm_autoencoder.add(TimeDistributed(Dense(n_features)))
+
+lstm_autoencoder.summary()
+adam = optimizers.Adam(lr)
+lstm_autoencoder.compile(loss='mse', optimizer=adam)
+
+cp = ModelCheckpoint(filepath="lstm_autoencoder_classifier.h5",
+                               save_best_only=True,
+                               verbose=0)
+
+tb = TensorBoard(log_dir='./logs',
+                histogram_freq=0,
+                write_graph=True,
+                write_images=True)
+
+lstm_autoencoder_history = lstm_autoencoder.fit(X_train_y0_scaled, X_train_y0_scaled, 
+                                                epochs=epochs, 
+                                                batch_size=batch, 
+                                                validation_data=(X_valid_y0_scaled, X_valid_y0_scaled),
+                                                verbose=2).history
+                                                
+plt.plot(lstm_autoencoder_history['loss'], linewidth=2, label='Train')
+plt.plot(lstm_autoencoder_history['val_loss'], linewidth=2, label='Valid')
+plt.legend(loc='upper right')
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.show()
+
+valid_x_predictions = lstm_autoencoder.predict(X_valid_scaled)
+mse = np.mean(np.power(flatten(X_valid_scaled) - flatten(valid_x_predictions), 2), axis=1)
+
+error_df = pd.DataFrame({'Reconstruction_error': mse,
+                        'True_class': y_valid.tolist()})
+
+precision_rt, recall_rt, threshold_rt = precision_recall_curve(error_df.True_class, error_df.Reconstruction_error)
+plt.plot(threshold_rt, precision_rt[1:], label="Precision",linewidth=5)
+plt.plot(threshold_rt, recall_rt[1:], label="Recall",linewidth=5)
+plt.title('Precision and recall for different threshold values')
+plt.xlabel('Threshold')
+plt.ylabel('Precision/Recall')
+plt.legend()
+plt.show()
+
+test_x_predictions = lstm_autoencoder.predict(X_test_scaled)
+mse = np.mean(np.power(flatten(X_test_scaled) - flatten(test_x_predictions), 2), axis=1)
+
+error_df = pd.DataFrame({'Reconstruction_error': mse,
+                        'True_class': y_test.tolist()})
+
+threshold_fixed = 0.3
+groups = error_df.groupby('True_class')
+fig, ax = plt.subplots()
+
+for name, group in groups:
+    ax.plot(group.index, group.Reconstruction_error, marker='o', ms=3.5, linestyle='',
+            label= "Break" if name == 1 else "Normal")
+ax.hlines(threshold_fixed, ax.get_xlim()[0], ax.get_xlim()[1], colors="r", zorder=100, label='Threshold')
+ax.legend()
+plt.title("Reconstruction error for different classes")
+plt.ylabel("Reconstruction error")
+plt.xlabel("Data point index")
+plt.show();
+
+pred_y = [1 if e > threshold_fixed else 0 for e in error_df.Reconstruction_error.values]
+conf_matrix = confusion_matrix(error_df.True_class, pred_y)
+
+plt.figure(figsize=(6, 6))
+sns.heatmap(conf_matrix, xticklabels=LABELS, yticklabels=LABELS, annot=True, fmt="d");
+plt.title("Confusion matrix")
+plt.ylabel('True class')
+plt.xlabel('Predicted class')
+plt.show()
+
+
+false_pos_rate, true_pos_rate, thresholds = roc_curve(error_df.True_class, error_df.Reconstruction_error)
+roc_auc = auc(false_pos_rate, true_pos_rate,)
+
+plt.plot(false_pos_rate, true_pos_rate, linewidth=5, label='AUC = %0.3f'% roc_auc)
+plt.plot([0,1],[0,1], linewidth=5)
+
+plt.xlim([-0.01, 1])
+plt.ylim([0, 1.01])
+plt.legend(loc='lower right')
+plt.title('Receiver operating characteristic curve (ROC)')
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
+
+            
